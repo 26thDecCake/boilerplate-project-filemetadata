24befc86a38c97f476eb92725466d8dc82874dbf
Romi Than <rrromithan@gmail.com>
2024-06-17 14:57:08 +0000

    fcc-file-metadata

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const multer = require('multer');
+const upload = multer();
+
 var app = express();
 
 app.use(cors());
@@ -12,7 +15,13 @@ app.get('/', function (req, res) {
 });
 
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req,res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
